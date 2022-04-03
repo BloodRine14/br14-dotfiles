3ec31eb841ba2e11540dd6713a51ba6a4ed91644
BloodRine14 <pratikkumar1406@gmail.com>
2022-04-03 14:19:10 +0530

    first push

---
+++ .xmonad/spotify.sh
@@ -0,0 +1,8 @@
+running=$(pidof spotify)
+if [ "$running" != "" ]; then
+    artist=$(playerctl metadata artist)
+    song=$(playerctl metadata title | cut -c 1-60)
+    echo -n "$song"
+else
+    echo -n "Off"
+fi
